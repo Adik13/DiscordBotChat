83cd56f75d4c0e11a2309f43c7d7c1a1592fd7a6
Andrey_M <52715086+Adik13@users.noreply.github.com>
2023-03-08 00:43:58 +0700

    Update run.py
    delete info

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app.discord_bot.connect_discord import client, discord_token
 from app.chatgpt_ai.connect_openai import openai
 
-openai.api_key='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'
-
 
 if __name__ == '__main__':
     client.run(discord_token)
